555e2e2b3d9b31793e5dbc46bf173f5295eab009
slislisrc <31811443+slislisrc@users.noreply.github.com>
2017-12-25 22:28:31 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,14 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
+  inverse1 <- NULL
   set <- function(y) {
     x <<- y
-    inv <<- NULL
+    inverse1 <<- NULL
   }
   get <- function() x
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
+  setinverse <- function(inverse) inverse1 <<- inverse
+  getinverse <- function() inverse1
   list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 
 }
@@ -24,13 +24,13 @@ cacheSolve <- function(x, ...) {
 }
 
 cacheSolve <- function(x, ...) {
-  inv <- x$getinverse()
-  if(!is.null(inv)) {
+  inverse1 <- x$getinverse()
+  if(!is.null(inverse1)) {
     message("getting cached data.")
-    return(inv)
+    return(inverse1)
   }
   data <- x$get()
-  inv <- solve(data)
-  x$setinverse(inv)
+  inverse1 <- solve(data)
+  x$setinverse(inverse1)
   inv
 }
